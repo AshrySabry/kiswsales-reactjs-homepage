7978f972b811907e74edc91bc964ff08b27cc8d8
Khairul Ashry Sabry <ashry@KISWIRE.MY>
2020-01-24 17:44:52 +0800

    responsive trial

--- src/components/SimpleCard.js
+++ src/components/SimpleCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -7,11 +8,28 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import Hidden from '@material-ui/core/Hidden';
+import withWidth from '@material-ui/core/withWidth';
 import Typography from '@material-ui/core/Typography';
 import './SimpleCard.css';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles( theme => ({
+
+    root: {
+      flexGrow: 1,
+    },
+
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+
     card: {
+      //padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      //flex: '1 0 auto',
+      //margin: theme.spacing(1),
       maxWidth: 450,
       margin: 'auto',
       marginTop: 80,
@@ -20,11 +38,12 @@ const useStyles = makeStyles({
     media: {
       height: 300,
     },
-  });
 
-const SimpleCard = () =>  {
+  }));
+
+const SimpleCard = (props) =>  {
   const classes = useStyles();
-
+  const {width} = props;
   return (
     <Grid container className={classes.root} spacing={2}>
     <Grid item xs={12} margin="auto">
@@ -44,7 +63,6 @@ const SimpleCard = () =>  {
           alt="Contemplative Reptile"
           height="200"
           image= {require ("./DJI_0031.JPG")}
-          
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2" className="text1-center">
@@ -52,8 +70,8 @@ const SimpleCard = () =>  {
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" position="center" className="text1-center">
+      <CardActions display={{ md: 'none' }} m={1}>
+        <Button size="small" color="primary" position="center" className="text1-center" >
           Login
         </Button>
         <Button size="small" color="primary">
@@ -66,5 +84,9 @@ const SimpleCard = () =>  {
     
   );
 }
+
+SimpleCard.propTypes = {
+  width: PropTypes.oneOf(['lg', 'md', 'sm', 'xl', 'xs']).isRequired,
+};
 
 export default SimpleCard;
